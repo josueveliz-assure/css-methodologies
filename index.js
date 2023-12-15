ca69173f6a55b0375eb2ddcca0291720c54870a2
JoDemVel <josuedemetriovelizescobar@gmail.com>
2023-12-15 18:12:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+
+$('.formMsg-link').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+ $('.loginForm__msg--link').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+$('.message .link').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
